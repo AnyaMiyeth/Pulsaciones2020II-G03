06d6c1345b1128209dc7eca7f01c6a513320e890
anyamiyeth <anyamiyeth@byasystems.com.co>
2020-09-16 08:57:05 -0500

    Agregar archivos de proyecto.

---
+++ Presentacion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            string nombre;
+            string sexo;
+            string identificacion;
+            decimal pulsacion;
+
+
+            Console.WriteLine("Digite la identificacion");
+            identificacion = Console.ReadLine();
+
+            Console.WriteLine("Digite el nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Digite el sexo F/M:");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("Digite la edad");
+            edad = int.Parse(Console.ReadLine());
+
+            if (sexo.ToUpper().Equals("F"))
+            {
+                pulsacion = (220 - edad) / 10;
+            }
+            else if (sexo.ToUpper().Equals("M"))
+            {
+                pulsacion = (210 - edad) / 10;
+            }
+            else
+            {
+                pulsacion = 0;
+            }
+
+            Console.WriteLine($"Su Pulsaciones {pulsacion} " );
+
+        }
+    }
+}
